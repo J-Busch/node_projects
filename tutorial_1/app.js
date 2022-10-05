267b4e8b1476a51b40b3d4c8b8a935a8d9555e16
Joel Busch <joelbusch94@gmail.com>
2022-10-05 17:11:14 -0500

    learned about html built in module

--- tutorial_1/app.js
+++ tutorial_1/app.js
@@ -1,27 +1,23 @@
-const {readFileSync, writeFileSync, readFile, writeFile} = require('fs')
+const http = require('http')
 
-readFile('./content/first.txt', 'utf8', (err, result)=>{
-    if (err) {
-        console.log(err)
-        return
+const PORT = 5000
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/') {
+        res.end('Welcome to our home page')
     }
-    const first = result
-    readFile('./content/second.txt', 'utf8', (err,result)=>{
-        if (err) {
-            console.log(err)
-            return
-        }
-        const second = result
-        writeFile(
-            './content/result-async.txt',
-            `Here is the result: ${first}, ${second}`,
-            (err, result)=>{
-                if(err){
-                    console.log(err)
-                    return
-                }
-                console.log(result)
-            }
-            )
-    })
+    else if(req.url === '/about') {
+        res.end('Here is our about page')
+    }
+    else {
+        res.end(`
+        <h1>There was a problem</h1>
+        <p>We can't find the page you are looking for</p>
+        <a href="/">Back home</a>
+        `)
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`)
 })
