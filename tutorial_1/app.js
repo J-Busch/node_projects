0eb2050a5808d221648e60030fe2957ac1d52394
Joel Busch <joelbusch94@gmail.com>
2022-10-05 18:03:30 -0500

    learned about nodemon and npm scripts

--- tutorial_1/app.js
+++ tutorial_1/app.js
@@ -2,6 +2,9 @@
 // manual approach (create package.json in the root, create properties, etc)
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
+// npm install (installs everything in package.json)
+// npm i <packageName> -D (dev dependency for dependencies not needed in production)
+// npm install -g <packageName> (global dependency)
 
 const _ = require('lodash')
 
