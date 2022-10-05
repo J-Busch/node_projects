fb78075a44f50f78ba1ff1a9faf451442b0f3997
Joel Busch <joelbusch94@gmail.com>
2022-10-05 13:22:34 -0500

    learned alternate way to work with modules

--- tutorial_1/app.js
+++ tutorial_1/app.js
@@ -11,7 +11,9 @@
 
 const names = require("./names")
 const sayHi = require("./utils")
+const data = require("./names_alt")
 
+console.log(data)
 sayHi("Joel")
 sayHi(names.john)
 sayHi(names.peter)
